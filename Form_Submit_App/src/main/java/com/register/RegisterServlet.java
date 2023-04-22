09bde2e197de9bf10205290652cc3c6e2a08816f
Afshal <afshal56@gmail.com>
2023-04-22 16:12:38 +0545

    Simple Registeration From

---
+++ Form_Submit_App/src/main/java/com/register/RegisterServlet.java
@@ -0,0 +1,29 @@
+package com.register;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RegisterServlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String name = req.getParameter("user_name");
+		String email = req.getParameter("email");
+		String password = req.getParameter("password");
+		String course = req.getParameter("course");
+
+		resp.setContentType("text/html");
+
+		PrintWriter out = resp.getWriter();
+		out.print("<h1>Name: " + name + "</h1>");
+		out.print("<h1>Name: " + email + "</h1>");
+		out.print("<h1>Name: " + password + "</h1>");
+		out.print("<h1>Name: " + course + "</h1>");
+	}
+
+}
