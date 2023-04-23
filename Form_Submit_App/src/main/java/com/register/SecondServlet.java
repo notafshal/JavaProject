e19cc7064de11d523c5e31d1c4aee569516f2f81
Afshal <afshal56@gmail.com>
2023-04-23 05:58:55 +0545

    update

--- Form_Submit_App/src/main/java/com/register/SecondServlet.java
+++ Form_Submit_App/src/main/java/com/register/SecondServlet.java
@@ -12,11 +12,12 @@ public class SecondServlet extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String email=req.getParameter("em");
 		
 		resp.setContentType("text/html");
 		PrintWriter out =resp.getWriter();
 		
-		out.print("<h1> Welcome to Home Page <h1>");
+		out.print("Your Email: "+ email +"<h1> Welcome to Home Page <h1>");
 	}
 
 }
