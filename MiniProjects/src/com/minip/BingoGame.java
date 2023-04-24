b348d8827a2dc91e701a0221bdd8753db7cec3f9
Afshal <afshal56@gmail.com>
2023-04-24 12:14:05 +0545

    Practice Projects

--- MiniProjects/src/com/minip/BingoGame.java
+++ MiniProjects/src/com/minip/BingoGame.java
@@ -5,21 +5,20 @@ import java.util.Scanner;
 
 public class BingoGame {
 	public static void main(String[] args) {
-	int num;	
-		
-		
-		Scanner sc= new Scanner(System.in);
+		int num;
+
+		Scanner sc = new Scanner(System.in);
 		Random r = new Random();
-		int rnumber=r.nextInt(100)+1;
+		int rnumber = r.nextInt(100) + 1;
 		System.out.println("***Welcome to Bingo***");
 		System.out.println("Enter your number for Bingo: ");
-		num=sc.nextInt();
-		System.out.println("The Bingo number is: "+ rnumber);
-		if(num<rnumber) {
+		num = sc.nextInt();
+		System.out.println("The Bingo number is: " + rnumber);
+		if (num < rnumber) {
 			System.out.println("Sorry! Your number is low");
-		}else if(num>rnumber){
+		} else if (num > rnumber) {
 			System.out.println("Sorry! Your numberis high");
-		}else {
+		} else {
 			System.out.println("Congratulations on winning the Bingo");
 		}
 
